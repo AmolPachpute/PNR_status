ec468e844fce664ad1b74b7895ec62c068648bb3
AmolPachpute <amolpachpute94@gmail.com>
2015-06-24 10:53:28 +0530

    pnr

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PNR_status
-Get pnr status 
+Get railway pnr status
+
+This small script uses railway api to get the status of the PNR number and sends message/status to the provided mobile numbers.
+You will need online messaging account with smsgupshup
--- pnr_status.py
+++ pnr_status.py
@@ -2,13 +2,15 @@ import urllib2
 import urllib
 import ipdb
 
-send_to_list = ['8050326214']
+send_to_list = ['9999999999', '8888888888']
 api_key = "12345"
+smsGupshupUserID = "userid"
+smsGupshupPwd = "pwd"
 
 def getPNR(pnr):
     req = urllib2.Request("http://api.railwayapi.com/pnr_status/pnr/"+pnr+"/apikey/"+api_key+"/")
     response = urllib2.urlopen(req)
-    #ipdb.set_trace()
+    ipdb.set_trace()
     response_dict = eval(response.read().replace("\n", ""))
     if response_dict["error"] == False :
         trainNumber = response_dict["train_num"]
@@ -24,14 +26,14 @@ def getPNR(pnr):
 
 def sendMessage(message):
     for i in send_to_list:
-        str1 = 'SendMessage'
         send_to= i
         message = urllib.quote(message)
-        url = "http://enterprise.smsgupshup.com/GatewayAPI/rest?method=SendMessage&send_to="+send_to+"&msg="+message+"&msg_type=TEXT&userid=2000123765&auth_scheme=plain&password=29Q1xu8LO+&v=1.1&format=text"
+        url = "http://enterprise.smsgupshup.com/GatewayAPI/rest?method=SendMessage&send_to="+send_to+"&msg="+message+"&msg_type=TEXT&userid="+smsGupshupUserID+"&auth_scheme=plain&password="+smsGupshupPwd+"+&v=1.1&format=text"
         req = urllib2.Request(url)
         response = urllib2.urlopen(req)
         response = response.read()
         print response
 
 
-getPNR('568978')
+getPNR('1232323')
+
