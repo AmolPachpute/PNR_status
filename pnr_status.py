2161043a08693f3215a0186e10206933d7705f15
AmolPachpute <amolpachpute94@gmail.com>
2015-07-14 14:19:22 +0530

    Update pnr_status.py

--- pnr_status.py
+++ pnr_status.py
@@ -2,6 +2,7 @@ import urllib2
 import urllib
 import ipdb
 
+# Change mobile numbers to which you want to send messages
 send_to_list = ['9999999999', '8888888888']
 api_key = "12345"
 smsGupshupUserID = "userid"
@@ -10,7 +11,8 @@ smsGupshupPwd = "pwd"
 def getPNR(pnr):
     req = urllib2.Request("http://api.railwayapi.com/pnr_status/pnr/"+pnr+"/apikey/"+api_key+"/")
     response = urllib2.urlopen(req)
-    ipdb.set_trace()
+   
+   # The response is in json format. Remove new line characters ans convert it to python dictionaries
     response_dict = eval(response.read().replace("\n", ""))
     if response_dict["error"] == False :
         trainNumber = response_dict["train_num"]
@@ -23,16 +25,21 @@ def getPNR(pnr):
                   + "From : " + fromStation + "\n" + "To : " + toStation + "PNR Status : " + pnrStatus
         sendMessage(message)
         return
+    else:
+        print "Cannot connect to url."
 
 def sendMessage(message):
-    for i in send_to_list:
-        send_to= i
-        message = urllib.quote(message)
-        url = "http://enterprise.smsgupshup.com/GatewayAPI/rest?method=SendMessage&send_to="+send_to+"&msg="+message+"&msg_type=TEXT&userid="+smsGupshupUserID+"&auth_scheme=plain&password="+smsGupshupPwd+"+&v=1.1&format=text"
-        req = urllib2.Request(url)
-        response = urllib2.urlopen(req)
-        response = response.read()
-        print response
+    try:
+        for i in send_to_list:
+            send_to= i
+            message = urllib.quote(message)
+            url = "http://enterprise.smsgupshup.com/GatewayAPI/rest?method=SendMessage&send_to="+send_to+"&msg="+message+"&msg_type=TEXT&userid="+smsGupshupUserID+"&auth_scheme=plain&password="+smsGupshupPwd+"+&v=1.1&format=text"
+            req = urllib2.Request(url)
+            response = urllib2.urlopen(req)
+            response = response.read()
+            print response
+    except:
+        print "Cannot connect to url"
 
 
 getPNR('1232323')
